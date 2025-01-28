635363f4cf78ae54d4e7366fad7e621eca8e3d33
a12lj <alyssoneskarleth22@gmail.com>
2025-01-28 12:38:50 -0500

    TRES EN RAYA
    TRES EN TRAAYA UTILIZANDO MATRICES EN JAVA.

---
+++ PROYECTO2025MATE/src/MATE2025PROYECTO/MATEPROYECTO.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package MATE2025PROYECTO;
+
+/**
+ * 
+ */
+public class MATEPROYECTO {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
